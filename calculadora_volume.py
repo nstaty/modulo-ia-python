e90e3986fb34ea2ae833de8f254777a76b3eed43
Tatiana Luz <nstaty@gmail.com>
2025-09-14 20:56:37 -0300

    Concluidos 4 novos exercicios

---
+++ calculadora_volume.py
@@ -0,0 +1,12 @@
+# Este programa calcula o volume de uma caixa retangular 
+
+# Solicita as dimensões da caixa ao usuário
+comprimento = 12
+largura = 14
+altura = 20
+
+# Calcula o volume da caixa
+volume = comprimento * largura * altura
+
+# Exibe o resultado
+print(volume)
---
+++ hello_world.py
@@ -0,0 +1,4 @@
+# Este programa imprime uma mensagem de saudação
+
+# imprime a mensagem na tela
+print ("olá, mundo")
