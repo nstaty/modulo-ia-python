c0069a5211b81f58c871a94f2ae6aaf5965e172f
Tatiana Luz <nstaty@gmail.com>
2025-09-14 23:51:23 -0300

    Calculadora verificar consumo de Combustível km/l

---
+++ atividade-pratica2/calculadora_consumo_combustivel.py
@@ -0,0 +1,14 @@
+# Calculadora de Consumo de Combustível
+
+# Dados
+distancia = 300  # km
+combustivel = 25  # litros
+
+# Cálculo
+consumo_medio = distancia / combustivel
+
+# Exibição
+print("=== Calculadora de Consumo de Combustível ===")
+print(f"Distância percorrida: {distancia} km")
+print(f"Combustível gasto: {combustivel} litros")
+print(f"Consumo médio: {consumo_medio:.2f} km/l")
