54db1368a33907e27ca0b6bb7bc07acf620e3da5
Tatiana Luz <nstaty@gmail.com>
2025-09-25 23:50:51 -0300

    Adiciona Atividade Pratica 07 (Ler csv)

---
+++ atividade-pratica7/escrever_csv.py
@@ -0,0 +1,16 @@
+import csv
+
+def criar_csv(nome_arquivo):
+    dados = [
+        ['Ana', 28, 'Rio de Janeiro'],
+        ['Pedro', 34, 'São Paulo'],
+        ['Maria', 30, 'Salvador']
+    ]
+
+    with open(nome_arquivo, 'w', newline='', encoding='utf-8') as arquivo_csv:
+        escritor = csv.writer(arquivo_csv)
+        escritor.writerow(['Nome', 'Idade', 'Cidade'])
+        for linha in dados:
+            escritor.writerow(linha)
+
+criar_csv("dados_teste.csv")
